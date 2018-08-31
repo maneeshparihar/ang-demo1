d350d16fb6e92aff30da6fa250215732a5304088
Maneesh Parihar <maneeshparihar@gmail.com>
2018-08-31 20:17:07 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+
   title = 'demo1';
+
+  public cards: Array<any> = [
+    {text: 'Card 1'},
+    {text: 'Card 2'},
+    {text: 'Card 3'},
+    {text: 'Card 4'},
+    {text: 'Card 5'},
+    {text: 'Card 6'},
+    {text: 'Card 7'},
+    {text: 'Card 8'},
+    {text: 'Card 9'},
+    {text: 'Card 10'}
+
+  ];
+
+  
+
+  addCard(cardText: String){
+    this.cards.push({text: cardText});
+    console.log(this.cards);
+  }
+
+
+
 }
